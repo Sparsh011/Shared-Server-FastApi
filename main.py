041c50006174db220512592ec206a1281e6c08b4
Sparsh Chadha <93155464+Sparsh011@users.noreply.github.com>
2024-03-11 14:56:33 +0530

    env var added and code refactor & cleanup

--- main.py
+++ main.py
@@ -1,35 +1,39 @@
+import os
+
 import requests
+from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
 
 import Constants
 
 app = FastAPI()
+load_dotenv()
 
 
-@app.get("/login/oauth/access_token")
-async def login(code: str):
+@app.get("/login")
+async def loginWithCode(code: str):
     url = Constants.ACCESS_TOKEN_BASE_URL + Constants.ACCESS_TOKEN_API_ROUTE
     headers = {
         "Accept": "application/json"
     }
     params = {
-        "client_secret": Constants.CLIENT_SECRET,
+        "client_secret": os.getenv(Constants.GITHUB_CLIENT_SECRET_ENV_KEY),
         "code": code,
-        "client_id": Constants.CLIENT_ID
+        "client_id": os.getenv(Constants.GITHUB_CLIENT_ID_ENV_KEY)
     }
     response = requests.post(url=url, headers=headers, params=params)
 
     if response.status_code == 200:
         data = response.json()
         if "access_token" in data:
-            return getUserDetailsFrom(access_token=data["access_token"])
+            return _getUserDetailsFrom(access_token=data["access_token"])
         else:
             raise HTTPException(status_code=400, detail="Access token not found in response")
     else:
         raise HTTPException(status_code=response.status_code, detail="Failed to get access token")
 
 
-def getUserDetailsFrom(access_token):
+def _getUserDetailsFrom(access_token):
     url = Constants.USER_DETAILS_BASE_URL + Constants.USER_DETAILS_API_ROUTE
 
     headers = {
